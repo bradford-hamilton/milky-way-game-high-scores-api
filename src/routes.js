d3344cd4f6914ba34d112e5cec952cde080baabd
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2017-08-11 19:27:41 -0600

    sketch out of the post route.. to be continued

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,14 @@ router.get('/high-scores', (req, res, next) => {
     .then((highScores) => {
       res.status(200).json(highScores);
     })
+    .catch((err) => console.log(err));
+});
+
+router.post('/new-score', (req, res, next) => {
+  // need userName and userScore to pass into below function...
+  // will it be coming in the body or params?
+  Scores.insertNewScore()
+
 });
 
 module.exports = router;
