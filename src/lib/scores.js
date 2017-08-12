3c6735426986979faa342b9959caf61d72d1b5b0
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2017-08-11 19:27:26 -0600

    add insertNewScore function that will work with the post route

--- src/lib/scores.js
+++ src/lib/scores.js
@@ -1,9 +1,18 @@
 import knex from '../../db/knex';
 
 export default class Scores {
+
   static getHighScores() {
     return knex('user_scores')
       .orderBy('score', 'desc')
       .limit(10);
+  }
+
+  static insertNewScore(userName, userScore) {
+    return knex('user_scores')
+      .insert({
+        name: userName,
+        score: userScore
+      });
   }
 }
